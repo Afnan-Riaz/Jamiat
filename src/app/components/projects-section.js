98293fa39c9310658e2325a442eff46dab2ef3ef
Afnan Riaz <hafiz.afnan322@gmail.com>
2023-11-18 13:04:06 +0500

    fixed swiper context error

--- src/app/components/projects-section.js
+++ src/app/components/projects-section.js
@@ -1,15 +1,8 @@
-import Swiper from "swiper";
-import { SwiperSlide } from "swiper/react";
 
 
 export default function Projects(){
     return(
         <div className="w-full h-screen">
-            <Swiper>
-                <SwiperSlide>
-                    
-                </SwiperSlide>
-            </Swiper>
         </div>
     )
 }
