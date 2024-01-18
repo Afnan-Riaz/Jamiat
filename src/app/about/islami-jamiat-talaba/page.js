8b6990df93e40351fa6f3ac03f320a59f9e49e55
Afnan Riaz <hafiz.afnan322@gmail.com>
2024-01-18 09:06:33 +0500

    error handling-6

--- src/app/about/islami-jamiat-talaba/page.js
+++ src/app/about/islami-jamiat-talaba/page.js
@@ -1,4 +1,4 @@
-// import Image from "next/image";
+import Image from "next/image";
 // export const getData = async () => {
 //     try {
 //         const data = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/api`).then(
@@ -18,73 +18,73 @@
 //         console.error("Error fetching data:", error.message);
 //     }
 // };
-// export default async function IJT() {
-//     let data = await getData();
-//     return (
-//         <div className="mb-20">
-//             <div className="bg-header-bg bg-cover w-full h-28 -mt-24"></div>
-//             <div className="min-h-[300px] bg-analytics-bg bg-cover flex items-center justify-center">
-//                 <h1 className="text-5xl text-white font-semibold font-inter">
-//                     Jamiat
-//                 </h1>
-//             </div>
-//             <div className="flex lg:flex-row flex-col mt-14 lg:gap-20 lg:px-20 px-5">
-//                 <main className="lg:w-2/3 w-full">
-//                     {
-//                         <div key={data._id}>
-//                             {/* <h4 className="text-3xl font-semibold text-blue-700 font-inter my-4">
-//                                 {section.heading}
-//                             </h4>
-//                             <p>{section.content}</p> */}
-//                             {data.content}
-//                         </div>
-//                     }
-//                 </main>
-//                 <aside className="lg:w-1/3 w-full">
-//                     <div className="w-full flex gap-5 items-center border-b-2 mt-5 pb-5">
-//                         <Image
-//                             src={"/inspiration-photo1.png"}
-//                             height={120}
-//                             width={120}
-//                             alt="photo"
-//                         />
-//                         <div>
-//                             <h4 className="text-lg font-medium">
-//                                 Syed Abul A'la Maududi example news
-//                             </h4>
-//                             <p className="text-sm text-gray-500">5 days ago</p>
-//                         </div>
-//                     </div>
-//                     <div className="w-full flex gap-5 items-center border-b-2 mt-5 pb-5">
-//                         <Image
-//                             src={"/inspiration-photo1.png"}
-//                             height={120}
-//                             width={120}
-//                             alt="photo"
-//                         />
-//                         <div>
-//                             <h4 className="text-lg font-medium">
-//                                 Syed Abul A'la Maududi example news
-//                             </h4>
-//                             <p className="text-sm text-gray-500">5 days ago</p>
-//                         </div>
-//                     </div>
-//                     <div className="w-full flex gap-5 items-center border-b-2 mt-5 pb-5">
-//                         <Image
-//                             src={"/inspiration-photo1.png"}
-//                             height={120}
-//                             width={120}
-//                             alt="photo"
-//                         />
-//                         <div>
-//                             <h4 className="text-lg font-medium">
-//                                 Syed Abul A'la Maududi example news
-//                             </h4>
-//                             <p className="text-sm text-gray-500">5 days ago</p>
-//                         </div>
-//                     </div>
-//                 </aside>
-//             </div>
-//         </div>
-//     );
-// }
+export default async function IJT() {
+    // let data = await getData();
+    return (
+        <div className="mb-20">
+            {/* <div className="bg-header-bg bg-cover w-full h-28 -mt-24"></div>
+            <div className="min-h-[300px] bg-analytics-bg bg-cover flex items-center justify-center">
+                <h1 className="text-5xl text-white font-semibold font-inter">
+                    Jamiat
+                </h1>
+            </div>
+            <div className="flex lg:flex-row flex-col mt-14 lg:gap-20 lg:px-20 px-5">
+                <main className="lg:w-2/3 w-full">
+                    {
+                        <div key={data._id}>
+                            {/* <h4 className="text-3xl font-semibold text-blue-700 font-inter my-4">
+                                {section.heading}
+                            </h4>
+                            <p>{section.content}</p> }
+                            {data.content}
+                        </div>
+                    }
+                </main>
+                <aside className="lg:w-1/3 w-full">
+                    <div className="w-full flex gap-5 items-center border-b-2 mt-5 pb-5">
+                        <Image
+                            src={"/inspiration-photo1.png"}
+                            height={120}
+                            width={120}
+                            alt="photo"
+                        />
+                        <div>
+                            <h4 className="text-lg font-medium">
+                                Syed Abul A'la Maududi example news
+                            </h4>
+                            <p className="text-sm text-gray-500">5 days ago</p>
+                        </div>
+                    </div>
+                    <div className="w-full flex gap-5 items-center border-b-2 mt-5 pb-5">
+                        <Image
+                            src={"/inspiration-photo1.png"}
+                            height={120}
+                            width={120}
+                            alt="photo"
+                        />
+                        <div>
+                            <h4 className="text-lg font-medium">
+                                Syed Abul A'la Maududi example news
+                            </h4>
+                            <p className="text-sm text-gray-500">5 days ago</p>
+                        </div>
+                    </div>
+                    <div className="w-full flex gap-5 items-center border-b-2 mt-5 pb-5">
+                        <Image
+                            src={"/inspiration-photo1.png"}
+                            height={120}
+                            width={120}
+                            alt="photo"
+                        />
+                        <div>
+                            <h4 className="text-lg font-medium">
+                                Syed Abul A'la Maududi example news
+                            </h4>
+                            <p className="text-sm text-gray-500">5 days ago</p>
+                        </div>
+                    </div>
+                </aside>
+            </div> */}
+        </div>
+    );
+}
--- src/app/literature/magazine/page.js
+++ src/app/literature/magazine/page.js
@@ -1,5 +1,5 @@
-// import Link from "next/link";
-// import Image from "next/image";
+import Link from "next/link";
+import Image from "next/image";
 
 // const getData = async () => {
 //     try {
@@ -19,61 +19,61 @@
 //     }
 // };
 
-// async function Magazine() {
-//     let magazines = await getData();
-//     return (
-//         <div>
-//             <div className="bg-header-bg bg-cover w-full h-28 -mt-24"></div>
-//             <div className="min-h-[300px] bg-analytics-bg bg-cover flex items-center justify-center bg-center">
-//                 <h1 className="text-5xl text-white font-semibold font-inter">
-//                     Hamqadam Magazine
-//                 </h1>
-//             </div>
-//             <div className="flex justify-between gap-3 mx-20 my-10">
-//                 <div className="mt-5">
-//                     <h2 className="text-4xl mb-6 text-blue-700 font-semibold">
-//                         Index
-//                     </h2>
-//                     <ul className="space-y-3 list-disc text-lg">
-//                         {magazines.map((magazine) => (
-//                             <li
-//                                 key={magazine._id}
-//                                 className="flex justify-between gap-x-20"
-//                             >
-//                                 {magazine.title}
-//                                 <Link
-//                                     href={magazine.link}
-//                                     className="underline"
-//                                 >
-//                                     Read Here
-//                                 </Link>
-//                             </li>
-//                         ))}
-//                     </ul>
-//                 </div>
-//                 <div>
-//                     <Link className="block mb-10" href={magazines[0].link}>
-//                         <Image
-//                             className="max-w-[350px] h-fit"
-//                             src={magazines[0].description}
-//                             width={350}
-//                             height={350}
-//                             alt="photo"
-//                         />
-//                     </Link>
-//                     <Link href={magazines[1].link}>
-//                         <Image
-//                             className="max-w-[350px] h-fit"
-//                             src={magazines[1].description}
-//                             width={350}
-//                             height={350}
-//                             alt="photo"
-//                         />
-//                     </Link>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
+async function Magazine() {
+    // let magazines = await getData();
+    return (
+        <div>
+            {/* <div className="bg-header-bg bg-cover w-full h-28 -mt-24"></div>
+            <div className="min-h-[300px] bg-analytics-bg bg-cover flex items-center justify-center bg-center">
+                <h1 className="text-5xl text-white font-semibold font-inter">
+                    Hamqadam Magazine
+                </h1>
+            </div>
+            <div className="flex justify-between gap-3 mx-20 my-10">
+                <div className="mt-5">
+                    <h2 className="text-4xl mb-6 text-blue-700 font-semibold">
+                        Index
+                    </h2>
+                    <ul className="space-y-3 list-disc text-lg">
+                        {magazines.map((magazine) => (
+                            <li
+                                key={magazine._id}
+                                className="flex justify-between gap-x-20"
+                            >
+                                {magazine.title}
+                                <Link
+                                    href={magazine.link}
+                                    className="underline"
+                                >
+                                    Read Here
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div>
+                    <Link className="block mb-10" href={magazines[0].link}>
+                        <Image
+                            className="max-w-[350px] h-fit"
+                            src={magazines[0].description}
+                            width={350}
+                            height={350}
+                            alt="photo"
+                        />
+                    </Link>
+                    <Link href={magazines[1].link}>
+                        <Image
+                            className="max-w-[350px] h-fit"
+                            src={magazines[1].description}
+                            width={350}
+                            height={350}
+                            alt="photo"
+                        />
+                    </Link>
+                </div>
+            </div> */}
+        </div>
+    );
+}
 
-// export default Magazine;
+export default Magazine;
