8ec73f19222339c0c10832058d9d5721d8bc2e47
Afnan Riaz <hafiz.afnan322@gmail.com>
2024-02-06 13:06:26 +0500

    inpiration page responsive

--- src/app/inspirations/[inspiration]/page.js
+++ src/app/inspirations/[inspiration]/page.js
@@ -14,41 +14,44 @@ async function Inspiration({ params }) {
     return (
         <div>
             <div className="bg-header-bg bg-cover w-full h-28 -mt-24"></div>
-            <div className="min-h-[300px] bg-analytics-bg bg-cover flex items-center justify-center bg-center">
-                <h1 className="text-5xl text-white font-semibold font-inter">
+            <div className="min-h-[200px] sm:min-h-[300px] bg-analytics-bg bg-cover flex items-center justify-center bg-center">
+                <h1 className="sm:text-5xl text-3xl text-white text-center font-semibold font-inter">
                     {data.name}
                 </h1>
             </div>
-            <div className="flex lg:mx-20 mx-10 my-10">
-                <div className="w-1/3 space-y-4">
+            <div className="flex gap-6 flex-col lg:flex-row lg:mx-20 mx-4 sm:mx-10 my-10">
+                <div className="w-full lg:w-1/3 flex flex-col sm:max-lg:flex-row gap-4">
                     <Image
                         src={data.image}
-                        width={350}
+                        width={320}
                         height={200}
                         alt="photo"
+                        className="sm:w-72 lg:w-80"
                     />
-                    <div className="flex gap-3">
-                        <h3 className="text-xl font-semibold">Name: </h3>
-                        <p className="text-lg">{data.name}</p>
-                    </div>
-                    <div className="flex gap-3">
-                        <h3 className="text-xl font-semibold">Born: </h3>
-                        <p className="text-lg">{data.dob}</p>
-                    </div>
-                    {data.dod && (
-                        <div className="flex gap-3">
-                            <h3 className="text-xl font-semibold">Died: </h3>
-                            <p className="text-lg">{data.dod}</p>
+                    <div className="space-y-4">
+                        <div className="flex gap-3 items-center">
+                            <h3 className="text-xl font-semibold">Name: </h3>
+                            <p className="text-base">{data.name}</p>
                         </div>
-                    )}
-                    <div className="flex gap-3">
-                        <h3 className="text-xl font-semibold">Designation: </h3>
-                        <p className="text-lg">{data.designation}</p>
+                        <div className="flex gap-3 items-center">
+                            <h3 className="text-xl font-semibold">Born: </h3>
+                            <p className="text-base">{data.dob}</p>
+                        </div>
+                        {data.dod && (
+                            <div className="flex gap-3 items-center">
+                                <h3 className="text-xl font-semibold">Died: </h3>
+                                <p className="text-base">{data.dod}</p>
+                            </div>
+                        )}
+                        <div className="flex gap-3 items-center">
+                            <h3 className="text-xl font-semibold">Designation: </h3>
+                            <p className="text-base">{data.designation}</p>
+                        </div>
                     </div>
                 </div>
-                <div className="w-2/3">
+                <div className="w-full lg:w-2/3">
                     <div>
-                        {/* <h2 className="text-blue-700 text-3xl font-semibold mb-3">
+                        <h2 className="text-blue-700 text-3xl font-semibold mb-3">
                             Heading 1
                         </h2>
                         <p className="mb-4">
@@ -71,7 +74,7 @@ async function Inspiration({ params }) {
                             corporis aliquid voluptatum, deserunt adipisci sunt
                             necessitatibus quod similique, totam aspernatur
                             nihil.
-                        </p> */}
+                        </p>
                         {data.content}
                     </div>
                 </div>
