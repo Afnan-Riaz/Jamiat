f3f8536a568788c7bc038ca246c5108ba3f9f3dc
Afnan Riaz <hafiz.afnan322@gmail.com>
2024-03-01 17:36:45 +0500

    font optimization

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,34 @@
-import Footer from './components/footer'
-import Header from './components/header'
-import './globals.css'
+import Footer from "./components/footer";
+import Header from "./components/header";
+import "./globals.css";
+import { Inter, Poppins } from "next/font/google";
 
-
+const inter = Inter({
+    subsets: ["latin"],
+    weight: ["700", "800", "900"],
+    variable: "--font-inter",
+    display: "swap",
+});
+const poppins = Poppins({
+    subsets: ["latin"],
+    weight: ["300", "400", "500", "600", "700", "900"],
+    variable: "--font-poppins",
+    display: "swap",
+});
 
 export const metadata = {
-  title: 'Islami Jamiat Talaba',
-  description: 'description',
-}
+    title: "Islami Jamiat Talaba",
+    description: "description",
+};
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className='m-0'>
-        <Header/>
-        {children}
-        <Footer/>
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en" className={`${inter.variable} ${poppins.variable}`}>
+            <body className="m-0">
+                <Header />
+                {children}
+                <Footer />
+            </body>
+        </html>
+    );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,44 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    fontFamily:{
-      sans:['Poppins', 'sans-serif'],
-      inter:['Inter', 'sans-serif'],
-      satisfy:['Satisfy','cursive'],
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+      extend: {
+            fontFamily: {
+                sans: ["var(--font-poppins)"],
+                inter: ["var(--font-inter)"],
+            },
+            backgroundImage: {
+                "header-bg": "url(/backgrounds/header-bg.webp)",
+                "card1-bg": "url(/backgrounds/card1-bg.webp)",
+                "card2-bg": "url(/backgrounds/card2-bg.webp)",
+                "card3-bg": "url(/backgrounds/card3-bg.webp)",
+                "card4-bg": "url(/backgrounds/card4-bg.webp)",
+                "long-card-bg": "url(/backgrounds/long-card-bg.webp)",
+                "about-bg": "url(/backgrounds/about-bg.webp)",
+                "journey-bg": "url(/backgrounds/journey-bg.webp)",
+                "inspiration-bg": "url(/backgrounds/inspiration-bg.webp)",
+                "message-bg": "url(/backgrounds/message-bg.webp)",
+                "footprint-bg": "url(/backgrounds/footprint-bg.webp)",
+                "analytics-bg": "url(/backgrounds/analytics-bg.webp)",
+                "media-bg": "url(/backgrounds/media-bg.webp)",
+                "subscribe-bg": "url(/backgrounds/subscribe-bg.webp)",
+            },
+        },
+        screens: {
+            laptop13: "1300px",
+            laptop12: "1200px",
+            mobile: "420px",
+            sm: "640px",
+            md: "768px",
+            lg: "1024px",
+            xl: "1280px",
+            "2xl": "1536px",
+        },
     },
-    extend: {
-      backgroundImage: {
-        'header-bg':"url(/backgrounds/header-bg.webp)",
-        'card1-bg':"url(/backgrounds/card1-bg.webp)",
-        'card2-bg':"url(/backgrounds/card2-bg.webp)",
-        'card3-bg':"url(/backgrounds/card3-bg.webp)",
-        'card4-bg':"url(/backgrounds/card4-bg.webp)",
-        'long-card-bg':"url(/backgrounds/long-card-bg.webp)",
-        'about-bg':"url(/backgrounds/about-bg.webp)",
-        'journey-bg':"url(/backgrounds/journey-bg.webp)",
-        'inspiration-bg':"url(/backgrounds/inspiration-bg.webp)",
-        'message-bg':"url(/backgrounds/message-bg.webp)",
-        'footprint-bg':"url(/backgrounds/footprint-bg.webp)",
-        'analytics-bg':"url(/backgrounds/analytics-bg.webp)",
-        'media-bg':"url(/backgrounds/media-bg.webp)",
-        'subscribe-bg':"url(/backgrounds/subscribe-bg.webp)",
-      },
-    },
-    screens:{
-      'laptop13':'1300px',
-      'laptop12':'1200px',
-      'mobile':'420px',
-      'sm':'640px',
-      'md':'768px',
-      'lg':'1024px',
-      'xl':'1280px',
-      '2xl':'1536px',
-    },
-  },
-  plugins: [],
-}
+    plugins: [],
+};
